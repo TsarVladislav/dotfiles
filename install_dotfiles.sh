420a4a254052ff34f204bbddaa38aa416b922aac
Vladislav Tsisyk <vlad@tsisyk.com>
2024-07-30 19:30:39 +0700

    install on fedora

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -8,23 +8,37 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.bash_git
 
 # bat
-sudo dnf install bat
+sudo dnf -y install bat
 
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-rm "$HOME"/.vimrc
-ln -s "$PWD"/.vimrc "$HOME"/.vimrc
+rm -f "$HOME"/.vimrc
+ln -sf "$PWD"/.vimrc "$HOME"/.vimrc
 
-rm "$HOME"/.tmux.conf
-ln -s "$PWD"/.tmux.conf "$HOME"/.tmux.conf
+rm -f "$HOME"/.tmux.conf
+ln -sf "$PWD"/.tmux.conf "$HOME"/.tmux.conf
 
 # bat
 mkdir -p ~/.local/bin
-ln -s /usr/bin/batcat ~/.local/bin/bat
+ln -sf /usr/bin/batcat ~/.local/bin/bat
 
-rm "$HOME"/.config/fish/config.fish
-ln -s "$PWD"/config.fish "$HOME"/.config/fish/config.fish
+# fish
+sudo dnf -y install fish
+
+rm -f "$HOME"/.config/fish/config.fish
+mkdir -p "$HOME"/.config/fish/
+
+ln -sf "$PWD"/config.fish "$HOME"/.config/fish/config.fish
 
 echo "source $HOME/dotfiles/bashrc" >> $HOME/.bashrc
+
+
+# install and update plugins
+vim +PluginUpdate +qall
+
+# theme
+
+cp -r $HOME/.vim/plugged/gruvbox/autoload/* $HOME/.vim/autoload/
+cp -r $HOME/.vim/plugged/gruvbox/colors/* $HOME/.vim/colors/
