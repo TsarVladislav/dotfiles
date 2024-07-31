dcef288a595b6ed0609a4436a47f5fc8b8500b18
Vladislav Tsisyk <vladislav.tsisyk@eltex.loc>
2024-07-31 09:46:21 +0700

    support for apt and dnf

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,8 +7,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # git
 curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.bash_git
 
+PACKAGE_MANAGER=""
+if [  -n "$(uname -a | grep Ubuntu)" ]; then
+    PACKAGE_MANAGER="apt"
+else
+    PACKAGE_MANAGER="dnf"
+fi
+
 # bat
-sudo dnf -y install bat
+sudo $PACKAGE_MANAGER -y install bat
+# fish
+sudo $PACKAGE_MANAGER -y install fish
 
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -24,9 +33,6 @@ ln -sf "$PWD"/.tmux.conf "$HOME"/.tmux.conf
 mkdir -p ~/.local/bin
 ln -sf /usr/bin/batcat ~/.local/bin/bat
 
-# fish
-sudo dnf -y install fish
-
 rm -f "$HOME"/.config/fish/config.fish
 mkdir -p "$HOME"/.config/fish/
 
@@ -34,11 +40,9 @@ ln -sf "$PWD"/config.fish "$HOME"/.config/fish/config.fish
 
 echo "source $HOME/dotfiles/bashrc" >> $HOME/.bashrc
 
-
 # install and update plugins
-vim +PluginUpdate +qall
+vim +PlugUpdate +qall
 
 # theme
-
 cp -r $HOME/.vim/plugged/gruvbox/autoload/* $HOME/.vim/autoload/
 cp -r $HOME/.vim/plugged/gruvbox/colors/* $HOME/.vim/colors/
